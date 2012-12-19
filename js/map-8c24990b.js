72381fa32a7d359b873318accc6020ca675ebb3f
Gpx <polvara@gmail.com>
2012-12-19 19:31:54 +0100

    first commit

---
+++ js/map-8c24990b.js
@@ -0,0 +1,1 @@
+$(function(){var e=new google.maps.LatLng(45.82117,9.358737),t={center:e,zoom:17,mapTypeId:google.maps.MapTypeId.ROADMAP,mapTypeControl:!1,draggable:!0},n=new google.maps.Map(document.getElementById("map_canvas"),t),r={animation:google.maps.Animation.DROP,flat:!1,map:n,position:e,visible:!0,title:"Trizero"},i=new google.maps.Marker(r)});
